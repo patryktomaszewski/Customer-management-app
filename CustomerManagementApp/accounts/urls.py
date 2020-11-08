fdf553362fa2411c0f266216d897897f0d27433d
patryktomaszewski <58734528+patryktomaszewski@users.noreply.github.com>
2020-11-08 19:23:26 +0100

    login and register pages

--- CustomerManagementApp/accounts/urls.py
+++ CustomerManagementApp/accounts/urls.py
@@ -6,9 +6,14 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('products/', views.products, name='products'),
     path('customer/<str:pk_test>', views.customer, name='customer'),
+
     path('create_order/<str:pk>', views.createOrder, name='create_order'),
     path('update_order/<str:pk>', views.updateOrder, name='update_order'),
     path('delete_order/<str:pk>', views.deleteOrder, name='delete_order'),
+
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
 
 
 ]
